1a8a3d612ddcd738591875163fc4602ab4c9e74d
Gizetto61 <127257041+Gizetto61@users.noreply.github.com>
2023-11-14 19:21:28 -0300

    Add files via upload

---
+++ PregaoGGG_Logica2.0/src/ControleDeDados/Pilha.java
@@ -0,0 +1,6 @@
+package ControleDeDados;
+
+public class Pilha
+{
+
+}
---
+++ PregaoGGG_Logica2.0/src/Entidades/Ativos.java
@@ -0,0 +1,68 @@
+package Entidades;
+
+public class Ativos
+{
+    private String Id;
+    private String Nome;
+    private double Valor;
+    private String Tipo;
+
+    public Ativos(String id, String nome, double valor, String tipo)
+    {
+        Id = id;
+        Nome = nome;
+        Valor = valor;
+        Tipo = tipo;
+    }
+
+    public String getId()
+    {
+        return Id;
+    }
+
+    public void setId(String id)
+    {
+        Id = id;
+    }
+
+    public String getNome()
+    {
+        return Nome;
+    }
+
+    public void setNome(String nome)
+    {
+        Nome = nome;
+    }
+
+    public double getValor()
+    {
+        return Valor;
+    }
+
+    public void setValor(double valor)
+    {
+        Valor = valor;
+    }
+
+    public String getTipo()
+    {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo)
+    {
+        Tipo = tipo;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Ativos{" +
+                "Id='" + Id + '\'' +
+                ", Nome='" + Nome + '\'' +
+                ", Valor=" + Valor +
+                ", Tipo='" + Tipo + '\'' +
+                '}';
+    }
+}
